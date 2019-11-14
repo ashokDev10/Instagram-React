d855b467760f468e09cf1cf96750b0d7ac6cd1bc
krist yohan <massashok420@gmail.com>
2019-11-14 17:20:03 +0530

    IGTV and it's footer added

--- src/Instagram/IGTV.jsx
+++ src/Instagram/IGTV.jsx
@@ -5,18 +5,37 @@ class IGTV extends Component {
   render() {
     return (
       <div className="IGTV-page">
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
-        <IGTVBlock image="https://placeimg.com/350/400/random" />
+        <div className="IGTV-container">
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+          <IGTVBlock image="https://placeimg.com/350/400/random" />
+        </div>
+        <div className="instagram-all-footer">
+          <ul>
+            <li>about us</li>
+            <li>Support</li>
+            <li>press</li>
+            <li>api</li>
+            <li>jobs</li>
+            <li>Privacy</li>
+            <li>terms</li>
+            <li>Directory</li>
+            <li>profiles</li>
+            <li>Hashtags</li>
+            <li>language</li>
+
+            <span>@ 2019 INSTAGRAM</span>
+          </ul>
+        </div>
       </div>
     );
   }
